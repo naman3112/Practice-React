2ec7f7b43305e7f50c7040fb09f9efe1d66766df
Naman Arora <namanarora311@gmail.com>
2021-08-23 15:23:53 +0530

    Use context

---
+++ src/Components/Us.js
@@ -0,0 +1,18 @@
+import React,{useEffect,useState} from 'react'
+
+function Us() {
+    const [msgObj, setMessage]=useState({message: '', id:1});
+    const handleChange=(e)=>{
+        let val=e.target.value;
+        let obj={...msgObj, message:val};
+        setMessage(obj)
+    }
+    return (
+        <div>
+            <input type='text' value={msgObj.message} onChange={handleChange}></input>
+            <p>{msgObj.message}</p>
+        </div>
+    )
+}
+
+export default Us
